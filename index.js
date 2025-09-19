9f2f4f294e372196c53d4b62d1ff4f7edee0a5fa
Ataar <mateenataar07@gmail.com>
2025-09-20 01:16:30 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 require("dotenv").config();
 
 const app = express();
@@ -10,13 +9,12 @@ app.use(cors());
 
 const Blog = require("./models/Blog");
 
-// ✅ Get all blogs
+// API Routes
 app.get("/blogs", async (req, res) => {
   const blogs = await Blog.find().sort({ createdAt: -1 });
   res.json(blogs);
 });
 
-// ✅ Create blog
 app.post("/blogs", async (req, res) => {
   try {
     const blog = new Blog(req.body);
@@ -27,26 +25,26 @@ app.post("/blogs", async (req, res) => {
   }
 });
 
-// ✅ Delete blog
 app.delete("/blogs/:id", async (req, res) => {
   await Blog.findByIdAndDelete(req.params.id);
   res.json({ message: "Deleted" });
 });
 
-// ✅ Like blog
 app.patch("/blogs/:id", async (req, res) => {
   const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true });
   res.json(blog);
 });
 
-// ✅ Serve frontend
+// Serve frontend
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 3000;
-
+// Connect DB and start server
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
-.catch(err => console.log(err));
+.then(() => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch(err => console.error(err));
