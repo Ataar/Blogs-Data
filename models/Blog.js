d1874b6c6a6f52b24ef243de371992cd5726e11a
Ataar <mateenataar07@gmail.com>
2025-09-20 01:02:38 +0530

    Initial commit

--- models/Blog.js
+++ models/Blog.js
@@ -1,9 +1,22 @@
+// const mongoose = require("mongoose");
+
+// const blogSchema = new mongoose.Schema({
+//   title: { type: String, required: true },
+//   category: String,
+//   content: String,
+//   authorId: String,
+//   likes: { type: Number, default: 0 }
+// }, { timestamps: true });
+
+// module.exports = mongoose.model("Blog", blogSchema);
+
+
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   category: String,
-  content: String,
+  content: { type: String, required: true },
   authorId: String,
   likes: { type: Number, default: 0 }
 }, { timestamps: true });
